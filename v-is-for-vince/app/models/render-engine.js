c50c325bbfe8b4d035de1aa07865d3abcea2f1c2
Dave Thomas <dave@assemblyco.com>
2016-12-02 22:01:55 -0800

    Coping game over

--- v-is-for-vince/app/models/render-engine.js
+++
@@ -1,2 +0,0 @@
-"use strict";System.register([],function(t,n){function e(t,n){if(!(t instanceof n))throw new TypeError("Cannot call a class as a function")}var r;return{setters:[],execute:function(){r=function t(){e(this,t)},t("default",r)}}});
-//# sourceMappingURL=render-engine.js.map
